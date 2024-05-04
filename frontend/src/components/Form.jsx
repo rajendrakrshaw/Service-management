d8a75d6bb34260ef3d422abfe7bc8e6fe4e59648
rajendrakrshaw <rajendra250601@gmail.com>
2024-05-04 11:24:54 +0530

    Completed form fields

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
-import { HiOutlineMail } from "react-icons/hi";
-import { MdOutlineHttp, MdOutlineContentCopy } from "react-icons/md";
+import { HiOutlineCalendar, HiOutlineMail } from "react-icons/hi";
+import {
+  MdOutlineHttp,
+  MdOutlineContentCopy,
+  MdOutlineAdd,
+  MdOutlineTextFields,
+  MdOutlineLocationCity,
+  MdOutlinePhone,
+  MdOutlineMail,
+  MdOutlinePassword,
+  MdOutlineVerified,
+} from "react-icons/md";
 import {
   TiSocialFacebook,
   TiSocialTwitter,
@@ -45,14 +55,14 @@ const Form = () => {
       <div className={Style.Form_box_mid}>
         <form onSubmit={handleSubmit}>
           <div className={Style.Form_section_title}>
-            <h1>Customer Details</h1>
+            <h1>Material Recieve</h1>
           </div>
           <div className={Style.Form_box_input}>
-            <label htmlFor="username">Username</label>
+            <label htmlFor="memono">Memo No</label>
             <input
               type="text"
-              name="username"
-              placeholder="shoaib bhai"
+              name="memono"
+              placeholder="#####"
               className={Style.Form_box_input_userName}
               onChange={handleInputChange}
               required
@@ -60,15 +70,15 @@ const Form = () => {
           </div>
 
           <div className={Style.Form_box_input}>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="reciecedate">Recieve Date & Time</label>
             <div className={Style.Form_box_input_box}>
               <div className={Style.Form_box_input_box_icon}>
-                <HiOutlineMail />
+                <HiOutlineCalendar />
               </div>
               <input
-                type="email"
-                name="email"
-                placeholder="Email*"
+                type="date"
+                name="recievedate"
+                placeholder="dd-mm-yyyy hh:mm:ss"
                 onChange={handleInputChange}
                 required
               />
@@ -76,27 +86,49 @@ const Form = () => {
           </div>
 
           <div className={Style.Form_box_input}>
-            <label htmlFor="description">Description</label>
-            <textarea
-              name="description"
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              className={Style.Form_box_input_userName}
+              onChange={handleInputChange}
+              required
+            />
+            {/* <textarea
+              name="name"
               cols="30"
               rows="6"
               placeholder="something about yourself in few words"
               onChange={handleInputChange}
               required
-            ></textarea>
+            ></textarea> */}
           </div>
 
           <div className={Style.Form_box_input}>
-            <label htmlFor="website">Website</label>
+            <label htmlFor="address">Address</label>
+            <div>
+              <textarea
+                name="address"
+                cols="30"
+                rows="6"
+                placeholder="your address"
+                onChange={handleInputChange}
+                required
+              ></textarea>
+            </div>
+          </div>
+
+          <div className={Style.Form_box_input}>
+            <label htmlFor="phone1">Phone No 1</label>
             <div className={Style.Form_box_input_box}>
               <div className={Style.Form_box_input_box_icon}>
-                <MdOutlineHttp />
+                <MdOutlinePhone />
               </div>
               <input
                 type="text"
-                name="website"
-                placeholder="website"
+                name="phone1"
+                placeholder="Phone No 1"
                 onChange={handleInputChange}
                 required
               />
@@ -104,15 +136,60 @@ const Form = () => {
           </div>
 
           <div className={Style.Form_box_input}>
-            <label htmlFor="facebook">Facebook</label>
+            <label htmlFor="phone2">Phone No 2</label>
             <div className={Style.Form_box_input_box}>
               <div className={Style.Form_box_input_box_icon}>
-                <TiSocialFacebook />
+                <MdOutlinePhone />
               </div>
               <input
                 type="text"
-                name="facebook"
-                placeholder="http://shoaib"
+                name="phone2"
+                placeholder="Phone No 2"
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+          </div>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="email">Email</label>
+            <div className={Style.Form_box_input_box}>
+              <div className={Style.Form_box_input_box_icon}>
+                <MdOutlineMail />
+              </div>
+              <input
+                type="email"
+                name="email"
+                placeholder="abc@xyz.com"
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+          </div>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="depositby">Deposit By</label>
+            <div>
+              <select
+                name="depositby"
+                className={Style.Form_box_input_userName}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Select an option</option>
+                <option value="bank">Self</option>
+                <option value="paypal">Engineer</option>
+                {/* <option value="creditcard">Credit Card</option> */}
+              </select>
+            </div>
+          </div>
+
+          <div className={Style.Form_box_input}>
+            <label htmlFor="item">Item</label>
+            <div className={Style.Form_box_input_box}>
+              <input
+                type="text"
+                className={Style.Form_box_input_userName}
+                name="item"
+                placeholder="e.g. CPU"
                 onChange={handleInputChange}
                 required
               />
@@ -120,58 +197,156 @@ const Form = () => {
           </div>
 
           <div className={Style.Form_box_input}>
-            <label htmlFor="twitter">Twitter</label>
+            <label htmlFor="accessories">Accessories Taken</label>
             <div className={Style.Form_box_input_box}>
-              <div className={Style.Form_box_input_box_icon}>
-                <TiSocialTwitter />
-              </div>
               <input
                 type="text"
-                name="twitter"
-                placeholder="http://shoaib"
+                className={Style.Form_box_input_userName}
+                name="accessories"
+                placeholder="e.g. mouse, keyboard etc..."
                 onChange={handleInputChange}
                 required
               />
+            </div>
+          </div>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="serialno">Serial No</label>
+            <input
+              type="text"
+              name="serialno"
+              placeholder="#####"
+              className={Style.Form_box_input_userName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="problems">
+              Problems {"("}For PC or Laptop Extra Details Required {")"}
+            </label>
+            <div>
+              <textarea
+                name="problems"
+                cols="30"
+                rows="6"
+                placeholder="e.g. Starting problem, booting problem, charging port problem etc."
+                onChange={handleInputChange}
+                required
+              ></textarea>
             </div>
           </div>
 
           <div className={Style.Form_box_input}>
-            <label htmlFor="instagram">Instagram</label>
+            <label htmlFor="billno">Bill/AMC No</label>
+            <input
+              type="text"
+              name="billno"
+              placeholder="#####"
+              className={Style.Form_box_input_userName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="billdate">Bill/AMC Date</label>
             <div className={Style.Form_box_input_box}>
               <div className={Style.Form_box_input_box_icon}>
-                <TiSocialInstagram />
+                <HiOutlineCalendar />
               </div>
               <input
-                type="text"
-                name="instagram"
-                placeholder="http://shoaib"
+                type="date"
+                name="billdate"
+                placeholder="dd-mm-yyyy"
                 onChange={handleInputChange}
                 required
               />
+            </div>
+          </div>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="warranty">Warranty</label>
+            <div Style={"display:flex;"}>
+              <label>
+                <input
+                  type="radio"
+                  name="warranty"
+                  value="yes"
+                  onChange={handleInputChange}
+                  required
+                />
+                Yes
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="warranty"
+                  value="no"
+                  onChange={handleInputChange}
+                  required
+                />
+                No
+              </label>
+              
+            </div>
+          </div>
+          <div className={Style.Form_box_input}>
+            <label htmlFor="standby">Stand By</label>
+            <div Style={"display:flex;"}>
+              <label>
+                <input
+                  type="radio"
+                  name="standby"
+                  value="yes"
+                  onChange={handleInputChange}
+                  required
+                />
+                Yes
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  name="standby"
+                  value="no"
+                  onChange={handleInputChange}
+                  required
+                />
+                No
+              </label>
+              
             </div>
           </div>
 
           <div className={Style.Form_box_input}>
-            <label htmlFor="wallet">Wallet address</label>
+            <label htmlFor="password">Pasword</label>
             <div className={Style.Form_box_input_box}>
               <div className={Style.Form_box_input_box_icon}>
-                <MdOutlineHttp />
+                <MdOutlinePassword />
               </div>
               <input
-                type="text"
-                name="wallet"
-                placeholder="0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8"
+                type="password"
+                name="password"
+                placeholder="37e3uyj099#*u"
                 onChange={handleInputChange}
                 required
               />
-              <div className={Style.Form_box_input_box_icon}>
-                <MdOutlineContentCopy />
-              </div>
             </div>
           </div>
-
+          <div className={Style.Form_box_input}>
+            <label htmlFor="signature">Recieved By Signature</label>
+            <div className={Style.Form_box_input_box}>
+              <div className={Style.Form_box_input_box_icon}>
+                <MdOutlineVerified />
+              </div>
+              <input
+                type="text"
+                name="signature"
+                placeholder=""
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+          </div>
           <div className={Style.Form_box_btn}>
-          <Button
+            <Button
               btnName="Upload profile"
               handleClick={handleSubmit}
               classStyle={Style.button}
